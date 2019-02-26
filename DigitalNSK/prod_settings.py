47d8e22d44a063fa51eebcbc800f51d7757da18f
David <slamvsem@gmail.com>
2019-02-26 21:09:38 +0700

    add media_root variable

--- DigitalNSK/prod_settings.py
+++ DigitalNSK/prod_settings.py
@@ -2,4 +2,4 @@ STATIC_ROOT = "/var/www/www-root/data/www/api.digitalnsk.sibtiger.com/static"
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = "/var/www/www-root/data/www/api.digitalnsk.sibtiger.com/media"
