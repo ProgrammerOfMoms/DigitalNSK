e2ccc18211a0e1280ffb1ab26090a0ad58700328
David <slamvsem@gmail.com>
2019-02-26 21:05:45 +0700

    fix static_root variable

---
+++ DigitalNSK/prod_settings.py
@@ -0,0 +1,5 @@
+STATIC_ROOT = "/var/www/www-root/data/www/api.digitalnsk.sibtiger.com/static"
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- DigitalNSK/settings.py
+++ DigitalNSK/settings.py
@@ -130,12 +130,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # REST Framework
 REST_FRAMEWORK = {
