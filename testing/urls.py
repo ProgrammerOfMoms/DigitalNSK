b797f0d375a159907500a4a26c4f1558d0a9211d
Dmitriy <drestbm@gmail.com>
2019-03-11 23:07:42 +0700

    fix tests

--- testing/urls.py
+++ testing/urls.py
@@ -4,8 +4,8 @@ from .views import *
 from .Create_false_tests import create_false_test
 
 urlpatterns = [
-    path('', create_false_test),
-    path('test1/', falseTest1.as_view()),
-    path('test2/', falseTest2.as_view()),
+    #path('', create_false_test),
+    path('test1/', Test1),
+    #path('test2/', falseTest2.as_view()),
     #path('test3/', falseTest3.as_view()),
 ]
