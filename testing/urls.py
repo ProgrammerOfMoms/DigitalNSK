e5628a6f1495eb08189c8327780cb2c3599cb898
Dmitriy <drestbm@gmail.com>
2019-03-12 22:53:18 +0700

    add doc

--- testing/urls.py
+++ testing/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('test1/', test1),
     path('test2/', test2),
     path('additional/', additional),
-    path('add-question/', test11),
+    path('result-additional/', resultOfAdditional),
     path('test3/', test3),
     path('result-test3/', resultOfTest3)
 ]
