0509fc58b920182176b69a33f79c059d4d6c47ce
Dmitriy <drestbm@gmail.com>
2019-03-29 00:25:37 +0700

    add urls

--- DigitalNSK/urls.py
+++ DigitalNSK/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include("user.urls")),
     path('tests/', include("testing.urls")),
-    path('event/', include("event.urls")),
+    path('event/', include("event.urls"))
 ]
 
 
