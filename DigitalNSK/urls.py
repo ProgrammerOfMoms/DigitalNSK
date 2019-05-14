3561ef4b8f9d5bb598038912dd8a066806de059c
Dmitriy <drestbm@gmail.com>
2019-05-14 22:00:51 +0700

    fix photo vk

--- DigitalNSK/urls.py
+++ DigitalNSK/urls.py
@@ -26,7 +26,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include("user.urls")),
-    path('tests/', include("testing.urls")),
+    path('', include("testing.urls")),
     path('event/', include("event.urls")),
     path('news/', include("news.urls"))
 ]
